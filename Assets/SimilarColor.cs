eddba8c12ba63c6cac180cd9881ac0d8df6c67c5
specsdhdeveloper <arthur.pointeau@gmail.com>
2019-09-02 23:55:30 +0200

    change color according to matching

---
+++ Assets/SimilarColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimilarColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {        
+        GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.white, Color.green, 1 - FindObjectOfType<FaceComparer>().expressionSimilarity/100f);
+	}
+}
