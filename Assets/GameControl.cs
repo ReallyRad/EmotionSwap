9a3e6e5dc388a1d4972d0e04cd424c0171c4b050
BeAnotherLabGit <beanotherlab@gmail.com>
2019-09-23 20:18:38 +0200

    skip when face lost

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -33,8 +33,8 @@ public class GameControl : MonoBehaviour {
 
     public void Skip()
     {
-        //GetComponent<VideoPlayer>().playbackSpeed = highSpeed;
         currentSpeed = highSpeed;
         waiting = false;
     }
+
 }
